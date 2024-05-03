a7ba31d65f834a2510e1dfbe3f154dd658812cc5
Wyeong6 <dnduddl1381@gmail.com>
2024-05-03 11:18:16 +0900

    test

--- app/src/main/java/com/example/oktodo/metro/BusanBusActivity.kt
+++ app/src/main/java/com/example/oktodo/metro/BusanBusActivity.kt
@@ -8,8 +8,12 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.oktodo.R
 
 class BusanBusActivity : AppCompatActivity() {
+
+    private lateinit var test: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_busan_bus)
+
+        test = "hello"
     }
 }
